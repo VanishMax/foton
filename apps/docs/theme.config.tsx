f530e4c32cf2243da0985838c7713f07aec05e22
vanishmax <maxkors2014@gmail.com>
2024-05-08 15:45:59 +0200

    feat(docs): create documentation app with Nextra

---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import type { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  logo: <span>Foton</span>,
+  project: {
+    link: 'https://github.com/VanishMax/foton',
+  },
+  docsRepositoryBase: 'https://github.com/VanishMax/foton',
+  footer: {
+    component: null,
+  }
+}
+
+export default config
