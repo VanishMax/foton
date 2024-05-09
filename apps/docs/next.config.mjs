7a86a46dd66e40a11640287d808c3976a3a38d2d
vanishmax <maxkors2014@gmail.com>
2024-05-09 13:11:05 +0200

    feat(docs): add the beginning of the tutorial article with syntax highlighting

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,8 +1,31 @@
 import nextra from 'nextra';
+import { BUNDLED_LANGUAGES, getHighlighter } from 'shiki';
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  mdxOptions: {
+    rehypePrettyCodeOptions: {
+      getHighlighter: (options) =>
+        getHighlighter({
+          ...options,
+          langs: [
+            ...BUNDLED_LANGUAGES.filter((language) => ['typescript', 'json', 'shellscript'].includes(language.id)),
+            {
+              id: 'tact',
+              scopeName: 'source.tact',
+              aliases: ['tact'],
+              path: path.join(__dirname, 'tact-grammar.json'),
+            }
+          ]
+        })
+    }
+  },
 });
 
 export default withNextra({});
